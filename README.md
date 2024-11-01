0f18a0a86f93fd409077b294978269f0991e25ab
SaadNasir92 <nas.saad53@gmail.com>
2024-10-31 22:47:17 -0500

    Finalized base views for powerbi integration and dashboard engineering. Updated project architecture for readme.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Spotify-ETL-Snowflake-PowerBI
 Spotify-Serverless-ETL-Pipeline-Engineering-with-AWS
+
+Currently in progress - below is an image of the pipeline being engineered.
+
+
+![Solution Architecture](resources/readme_imgs/solution_flow.png)
+
+
--- snowflake_queries/analytic_queries.sql
+++ snowflake_queries/analytic_queries.sql
@@ -1,49 +1,22 @@
--- total record check view creation
-create or replace view total_record_check as(
-with alb as (
-select count(*) as total_albums
-from albums
-),
-art as (
-select count(*) as total_artists
-from artists
-),
-song as (
-select count(*) as total_songs
-from songs
-)
-select
-    alb.total_albums,
-    art.total_artists,
-    song.total_songs
-from alb, art, song);
-
--- Top 5 artists with most songs in top 50 playlist
-create or replace view top_5_artists as (
 select a.artist_name, count(*) as "Total tracks in top 50" 
 from songs s
 join artists a
 on a.artist_id = s.artist_id
 group by a.artist_name
 order by "Total tracks in top 50" desc
-limit 5)
+limit 5
 ;
 
--- Avg popularity for songs added in last quarter vs older songs.
-create or replace view time_popularity_analysis as (
+
 with quarter_analysis as(
 select 
-s.popularity,
+*, 
 case when extract (month, added_date) >= extract (month, getdate()) - 3 then 'yes' else 'no' end as last_quarter
-from songs s)
-
-select last_quarter, avg(POPULARITY) as average_popularity 
-from quarter_analysis
-group by last_quarter);
+from songs)
+select last_quarter, avg(popularity) from quarter_analysis,
+group by last_quarter;
 
 
--- top 5 and bot 5 artists by popularity, song duration & track appearances.
-create or replace view top_bot_artists_analytics as (
 with cte1 as (
 select 
     a.artist_name,
@@ -79,20 +52,8 @@ select
     "Track Appearances in Top 50",
     "Total Tracks in Album"
 from final_table
-where ranking <=5 or ranking >= (select (max(ranking) - 5) as lowest_rank from final_table))
+where ranking <=5 or ranking >= (select (max(ranking) - 5) as lowest_rank from final_table)
 ;
-
-
-
-
-select * from time_popularity_analysis;
-select * from top_5_artists;
-select * from top_bot_artists_analytics;
-
-
-
-
-
 
 
 
